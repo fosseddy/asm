5751c2f90b4a239638c3e7bc5ebc2e771d096e40
fosseddy <markfosseddy@gmail.com>
2022-10-27 20:48:37 +0500

    hackish skip punctuation

--- palindrome.asm
+++ palindrome.asm
@@ -3,7 +3,7 @@ section .data
     FALSE equ 0
     NULL equ 0
 
-    msg db "annas", NULL
+    msg db "a man, a plan, a canal – panama!", NULL
     result dd TRUE
 
 section .text
@@ -12,9 +12,14 @@ _start:
     mov rdx, 0
     jmp .loop_1_test
 .loop_1:
+    cmp byte [msg+rdx], "a"
+    jl .loop_1_continue
+    cmp byte [msg+rdx], "z"
+    jg .loop_1_continue
     mov rax, 0
     mov al, [msg+rdx]
     push rax
+.loop_1_continue:
     inc rdx
 .loop_1_test:
     cmp byte [msg+rdx], NULL
@@ -23,14 +28,18 @@ _start:
     mov rdx, 0
     jmp .loop_2_test
 .loop_2:
+    cmp byte [msg+rdx], "a"
+    jl .loop_2_continue
+    cmp byte [msg+rdx], "z"
+    jg .loop_2_continue
     pop rax
     cmp al, [msg+rdx]
     jne .not_equals
-    jmp .continue
+    jmp .loop_2_continue
 .not_equals:
     mov dword [result], FALSE
     jmp .exit
-.continue:
+.loop_2_continue:
     inc rdx
 .loop_2_test:
     cmp byte [msg+rdx], NULL

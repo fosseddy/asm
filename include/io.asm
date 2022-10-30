da61a03b6dcc4c91154a36ee432fcd7e9cf66a35
fosseddy <markfosseddy@gmail.com>
2022-10-30 19:53:39 +0500

    io package

---
+++ include/io.asm
@@ -0,0 +1,17 @@
+section .text
+    global print
+    extern strlen
+
+; void print(byte *s)
+print:
+    call strlen
+    mov r10, rax
+    mov r11, rdi
+
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, r11
+    mov rdx, r10
+    syscall
+
+    ret
